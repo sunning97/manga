0ba1675d9ffe857e6cd645e8c037aa907d5af657
sunning97 <mrcatbro97@gmail.com>
2018-07-24 22:00:24 +0700

    update

--- app/Notifications/AdminResetPassword.php
+++ app/Notifications/AdminResetPassword.php
@@ -42,10 +42,10 @@ class AdminResetPassword extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('Bạn nhận được email này vì chúng tôi nhận được yêu cầu đặt lại mật khẩu cho tài khoản của bạn.')
-                    ->action('Xác nhận', route('admin.password.reset.request',$this->token))
-                    ->line('Nếu bạn không yêu cầu đặt lại mật khẩu, sẽ không có hành động yêu cầu này');
+        return (new MailMessage)->markdown('email.reset-password',['token' => $this->token]);
+//                    ->line('Bạn nhận được email này vì chúng tôi nhận được yêu cầu đặt lại mật khẩu cho tài khoản của bạn.')
+//                    ->action('Xác nhận', route('admin.password.reset.request',$this->token))
+//                    ->line('Nếu bạn không yêu cầu đặt lại mật khẩu, sẽ không có hành động yêu cầu này');
     }
 
     /**
