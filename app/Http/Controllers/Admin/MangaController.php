38a15ea4f27aba8014e80e8c408dd23312493dc8
sunning97 <mrcatbro97@gmail.com>
2018-08-16 20:58:36 -0700

    update

--- app/Http/Controllers/Admin/MangaController.php
+++ app/Http/Controllers/Admin/MangaController.php
@@ -179,6 +179,7 @@ class MangaController extends Controller
         $poster = Admin::find($manga->post_by);
         $editors = $manga->admins;
         $genres = $manga->genres;
+        
         return view('admin.mangas.show')->withManga($manga)->withChaps($chaps)->withPoster($poster)->withEditors($editors);
     }
 
